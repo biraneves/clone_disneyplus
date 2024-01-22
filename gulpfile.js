f3bed76b716c079b2a465e527cbc9d629b35a375
Ubirajara Neves <ubirajara.neves@usp.br>
2024-01-22 13:32:37 +0100

    Configuração do uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,26 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require ('sass'));
+const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles() {
-  return gulp.src('./src/assets/styles/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('./dist/assets/styles'));
+    return gulp
+        .src('./src/assets/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/assets/styles'));
 }
 
 function images() {
-  return gulp.src('./src/assets/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/assets/images'));
+    return gulp.src('./src/assets/images/**/*').pipe(imagemin()).pipe(gulp.dest('./dist/assets/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+function scripts() {
+    return gulp.src('./src/assets/js/*.js').pipe(uglify()).pipe(gulp.dest('./dist/assets/js'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function () {
-  gulp.watch('./src/assets/styles/*.scss', gulp.parallel(styles));
-}
+    gulp.watch('./src/assets/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/assets/js/*.js', gulp.parallel(scripts));
+};
